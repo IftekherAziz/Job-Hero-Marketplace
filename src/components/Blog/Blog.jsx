dd37e32b8b9bee089e8e4e3e58ad4ba7c3c1c498
Iftekher Aziz <azizchy02@gmail.com>
2023-04-10 23:52:13 +0600

    Design blog page and anwser question

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import BlogPost from './BlogPost/BlogPost';
 
 const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch("/public/blogs.json")
+          .then((res) => res.json())
+          .then((data) => setBlogs(data.blog_posts));
+    }, [])
+
     return (
-        <div>
-            <h2>Blog Page</h2>
+      <div className="mx-auto max-w-7xl px-8 mt-20 pb-20">
+        <div className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3">
+        {
+            blogs.map(blog => <BlogPost key={blog.id} blog={blog}></BlogPost>)
+        }    
         </div>
+      </div>
     );
 };
 
